3e8358e3d7931715dea56fe38a6ffe8ef0071f17
duke <dukeluo@outlook.com>
2019-11-19 23:02:31 +0800

    feat: >_<

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss'
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <p>Are you rich?</p>
+            </div>
+        );
+    }
+}
+
+export default App;
