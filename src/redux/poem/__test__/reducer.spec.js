047aa8e4a17ddc493eed0d99a8139460313296b8
duke <dukeluo@outlook.com>
2019-12-28 18:19:33 +0800

    test: test poem reducer

---
+++ src/redux/poem/__test__/reducer.spec.js
@@ -0,0 +1,27 @@
+import poemReducer from '../reducer';
+import * as types from '../type';
+
+describe('The poem reducer', () => {
+
+    it('should return the initial state', () => {
+        expect(poemReducer(undefined, {})).toEqual({
+            poem: '',
+        });
+    });
+
+    it('should handle GET_A_POEM', () => {
+        const content = '道可道，非常道。';
+        const action = {
+            type: types.GET_A_POEM,
+            payload: content,
+        };
+
+        expect(poemReducer(undefined, action)).toEqual({
+            poem: content,
+        });
+        expect(poemReducer({ poem: '锄禾日当午' }, action)).toEqual({
+            poem: content,
+        });
+    });
+
+});
