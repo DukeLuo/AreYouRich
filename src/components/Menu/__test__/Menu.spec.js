d25f396af5f8ca4f70c629d620e322bff93b91f7
duke <dukeluo@outlook.com>
2019-12-28 18:53:14 +0800

    refactor: refactor test of home page and menu component

--- src/components/Menu/__test__/Menu.spec.js
+++ src/components/Menu/__test__/Menu.spec.js
@@ -1,59 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
-import { render } from '@testing-library/react';
+import { renderWithRouter } from '../../../utils/testHelper';
 import Menu from '../index';
 
 import '@testing-library/jest-dom/extend-expect';
 
 describe('The Menu component', () => {
 
-    it('should have four items', () => {
-        const { getAllByTestId } = render(
-            <Router>
-                <Menu />
-            </Router>
-        );
+    let getAllByTestId;
 
+    beforeEach(() => {
+        ({ getAllByTestId } = renderWithRouter(<Menu />));
+    });
+
+    it('should have four items', () => {
         expect(getAllByTestId('item')).toHaveLength(4);
     });
 
     it('should have "应急能力检测"', () => {
-        const { getAllByTestId } = render(
-            <Router>
-                <Menu />
-            </Router>
-        );
-
         expect(getAllByTestId('item')[0]).toHaveTextContent('应急能力检测');
     });
 
     it('should have "偿债能力检测"', () => {
-        const { getAllByTestId } = render(
-            <Router>
-                <Menu />
-            </Router>
-        );
-
         expect(getAllByTestId('item')[1]).toHaveTextContent('偿债能力检测');
     });
 
     it('should have "储蓄能力检测"', () => {
-        const { getAllByTestId } = render(
-            <Router>
-                <Menu />
-            </Router>
-        );
-
         expect(getAllByTestId('item')[2]).toHaveTextContent('储蓄能力检测');
     });
 
     it('should have "资产生息能力检测"', () => {
-        const { getAllByTestId } = render(
-            <Router>
-                <Menu />
-            </Router>
-        );
-
         expect(getAllByTestId('item')[3]).toHaveTextContent('资产生息能力检测');
     });
 
---
+++ src/pages/home/__test__/home.spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { renderWithRouter } from '../../../utils/testHelper';
+import { Home } from '..';
+
+import '@testing-library/jest-dom/extend-expect';
+
+describe('The home page', () => {
+
+    it('should show a poem', () => {
+        const content = '道可道，非常道。';
+        const props = {
+            poem: content,
+            getPoem: jest.fn(),
+        };
+        const { getByTestId } = renderWithRouter(<Home {...props} />);
+
+        expect(props.getPoem).toBeCalledTimes(1);
+        expect(getByTestId('home')).toHaveTextContent(content);
+    });
+
+});
